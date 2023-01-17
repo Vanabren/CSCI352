2ccee643ab920a31b6e8f9fc3976d35d8a5a21a8
vanabren <vanabren@EPSG1-825026>
2023-01-17 13:36:33 -0600

    Add project files.

---
+++ CW2 - CSCI352 (Remake)/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Name: Vance Brender-A-Brandis
+   Date: 1/17/23
+   
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW2___CSCI352__Remake_
+{
+    class Program // change the name of the Class
+    {
+        private int steve;
+
+        public void setSteve(int s)
+        {
+            steve = s;
+        }
+
+        public int getSteve()
+        {
+            return steve;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!" + 5);
+            Console.WriteLine("setting steve to 4");
+            Program p = new Program();
+            p.setSteve(4);
+            Console.WriteLine("Steve is " + p.getSteve());
+            Console.ReadKey();
+        }
+    }
+}
